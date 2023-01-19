f92ced5102388771b4694be158f93148b263eee3
Ayush Soni <ayushsonirbt@gmail.com>
2023-01-20 00:08:28 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '18c4991173msh74e6ee5ee1c7431p18439cjsn31f5aa1b5a82',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5ea6de3dbeae01b45826b68f5ef1bf68"; // enter your key from openweather API
+
+
